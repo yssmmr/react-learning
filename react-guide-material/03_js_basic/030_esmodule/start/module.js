07684f8fdaaeb05d7a2f2f5b0c11c91369093f19
yssmmr <noppie429@gmail.com>
2023-12-11 23:38:27 +0900

    ADD モジュール

--- react-guide-material/03_js_basic/030_esmodule/start/module.js
+++ react-guide-material/03_js_basic/030_esmodule/start/module.js
@@ -2,15 +2,21 @@ export const hello = () => {
   console.log("hello!");
 };
 
+//export→外部からアクセス可能な変数に
+
 const funcB = () => {
   console.log("funcB output");
 };
 
 export default funcB;
+//defaultでimportできるように。ファイルに一つだけ。
 
 class User {
   constructor(name) {
     this.name = name;
+  }
+  hello() {
+    console.log(this.name);
   }
 }
 
