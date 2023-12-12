73eaff10f2ffabd8bbd208b39f81bb4591f07fda
yssmmr <noppie429@gmail.com>
2023-12-12 15:02:11 +0900

    ADD callback

--- react-guide-material/03_js_basic/040_callback/start/main.js
+++ react-guide-material/03_js_basic/040_callback/start/main.js
@@ -1,15 +1,11 @@
-function fn(number) {
+function print(callback) {
+  console.log(callback);
+  const result = callback();
+  console.log(result);
+  }
+
+function fn(number = 3) {
     return number * 2;
   }
-  
-  console.log(fn(2));
-  console.log(fn(4));
-  
-  const fnArrow = number => {
-    console.log(number);
-    return number * 2;
-  };
-  
-  const fnArrowObj = number => ({ result: number * 2 })
-  
-  console.log(fnArrowObj(2));
+
+print(fn);
